4fc3d9d6c986a26be6570d3d7c39a61929d54bd5
Tayeem00789 <tayeemextreme@gmail.com>
2026-08-02 21:32:21 +0600

    Module 1 Lesson 3

---
+++ module 1/Lesson 2/activityHW.py
@@ -0,0 +1,11 @@
+samiul = "29th October"
+musa = "20th July"
+sowad = "9th November"
+surjo = "15th August"
+tasin = "4th January"
+
+print(f"The Birthday of Samiul is {samiul}.")
+print(f"The Birthday of Musa is {musa}.")
+print(f"The Birthday of Sowad is {sowad}.")
+print(f"The Birthday of Surjo is {surjo}.")
+print(f"The Birthday of Tasin is {tasin}.")
